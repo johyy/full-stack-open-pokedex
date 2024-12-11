4fa6a277f8c68357c7fa409814b7b74fb9b75258
Jonna Hyypiä <jonna.hyypia@helsinki.fi>
2024-12-11 15:53:51 +0200

    Try failing the build message

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ const App = () => {
 
   if (match && match.params) {
     const pokemonId = pokemonList.find(({ name }) => name === match.params.name).id
-    previous = pokemonList.find(({ id }) => id === pokemonId - 1)
+    previous = pokemonLieeest.find(({ id }) => id === pokemonId - 1)
     next = pokemonList.find(({ id }) => id === pokemonId + 1)
   }
 
